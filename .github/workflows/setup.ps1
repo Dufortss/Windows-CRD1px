bd3578949fc474c1f7f9c015c956d55c574c56cf
Caio <caiosales001@gmail.com>
2024-12-19 00:09:00 -0300

    Update setup.ps1

--- .github/workflows/setup.ps1
+++ .github/workflows/setup.ps1
@@ -1,21 +1,3 @@
-# Desativar o firewall temporariamente (não recomendado em ambientes públicos)
 Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
-
-# Baixar e instalar o Chrome Remote Desktop
-& {
-  $P = $env:TEMP + '\chromeremotedesktophost.msi'
-  Invoke-WebRequest 'https://dl.google.com/edgedl/chrome-remote-desktop/chromeremotedesktophost.msi' -OutFile $P
-  Start-Process $P -Wait
-  Remove-Item $P
-}
-
-# Baixar e instalar o Google Chrome
-& {
-  $P = $env:TEMP + '\chrome_installer.exe'
-  Invoke-WebRequest 'https://dl.google.com/chrome/install/latest/chrome_installer.exe' -OutFile $P
-  Start-Process -FilePath $P -Args '/install' -Verb RunAs -Wait
-  Remove-Item $P
-}
-
-# Reativar o firewall após a execução
-Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled True
+& {$P = $env:TEMP + '\chromeremotedesktophost.msi'; Invoke-WebRequest 'https://dl.google.com/edgedl/chrome-remote-desktop/chromeremotedesktophost.msi' -OutFile $P; Start-Process $P -Wait; Remove-Item $P}
+& {$P = $env:TEMP + '\chrome_installer.exe'; Invoke-WebRequest 'https://dl.google.com/chrome/install/latest/chrome_installer.exe' -OutFile $P; Start-Process -FilePath $P -Args '/install' -Verb RunAs -Wait; Remove-Item $P}
